74aa8ade433763ad1803219f4dee647b738728f4
Charlie Merland <charlie@caercam.org>
2019-10-04 16:03:42 +0200

    Add French translation

---
+++ resources/lang/fr/log.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'new_permission' => 'Création d\'une nouvelle permission « « :name » ».',
+    'updated_permission' => 'Mise à jour de la permission « « :name » ».',
+    'deleted_permission' => 'Suppression de la permission « :name ».',
+
+    'new_role' => 'Création du rôle « :name ».',
+    'updated_role' => 'Mise à jour du rôle « :name ».',
+    'deleted_role' => 'Suppression du rôle « :name ».',
+    'updated_role_permissions' => 'Mise à jour des permission du rôle.',
+
+    'logged_in' => 'Connecté.',
+    'logged_out' => 'Déconnecté.',
+    'created_account' => 'Inscription.',
+    'updated_avatar' => 'Mise à jour de l’avatar.',
+    'updated_profile' => 'Mise à jour du profil.',
+    'deleted_user' => 'Suppression de l\'utilisateur « :name ».',
+    'banned_user' => 'Désactivation de l\'utilisateur « :name ».',
+    'updated_profile_details_for' => 'Mise à jour du profil de « :name ».',
+    'created_account_for' => 'Création d\'un compte pour l\'utilisateur « :name ».',
+    'updated_settings' => 'Mise à jour des réglages.',
+    'enabled_2fa' => 'Activation de la double authentication.',
+    'disabled_2fa' => 'Désactivation de la double authentication.',
+    'enabled_2fa_for' => 'Activation de la double authentication pour « :name ».',
+    'disabled_2fa_for' => 'Désactivation de la double authenticationpour « :name ».',
+    'requested_password_reset' => 'Demande de réinitialisation de mot de passe.',
+    'reseted_password' => 'Réinitialisation du mot de passe (« J\'ai perdu mon mot de passe »).',
+
+    'started_impersonating' => 'A pris l\'apparence de « :name » (ID: :id).',
+    'stopped_impersonating' => 'A repris son apparence normale.',
+];
